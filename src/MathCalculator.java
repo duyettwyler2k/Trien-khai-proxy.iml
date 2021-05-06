9ce826c071fd88c0076a5fe3060d0b59ff51761a
duyettwyler2k <duyettwyller@gmail.com>
2021-05-06 14:50:58 +0700

    'dfsd'

---
+++ src/MathCalculator.java
@@ -0,0 +1,22 @@
+public class MathCalculator implements Caculator{
+
+    @Override
+    public double add(double first, double second) {
+        return 0;
+    }
+
+    @Override
+    public double sub(double first, double second) {
+        return 0;
+    }
+
+    @Override
+    public double mul(double first, double second) {
+        return 0;
+    }
+
+    @Override
+    public double div(double first, double second) {
+        return 0;
+    }
+}
